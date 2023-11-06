8f3f9db71f38fa2e3370dffd93976b47c9786f8e
Th0m4sG <thomas.georgiou@phiture.com>
2023-11-06 10:55:08 +0200

    updated text on days climbing outside

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def user_input_features():
     repeaters = st.sidebar.number_input("7:3 hangs on a 20mm edge (total time in seconds)", min_value=1.0, max_value=1000.0, value=120.0,step=1.0, key="repeaters")
     trainexp = st.sidebar.number_input("Years of specific training for climbing", min_value=1.0, max_value=50.0, value=3.0,step=0.5, key="trainexp")
     exp = st.sidebar.number_input("Years of climbing experience", min_value=0.0, max_value=50.0, value=5.0,step=0.5, key="exp")
-    days = st.sidebar.number_input("Number of days spent climbing outside per month", min_value=0.0, max_value=31.0, value=8.0,step=1.0, key="days")
+    days = st.sidebar.number_input("Number of days spent climbing each month", min_value=0.0, max_value=31.0, value=8.0,step=1.0, key="days")
     
     # Calculate strength-to-weight ratios
     strength_to_weight_pullup =  max_pullups / weight
@@ -176,7 +176,7 @@ st.header("Predicted Grades")
 st.subheader(f"Max Boulder Grade: {bouldering_predicted_grade}")
 st.subheader(f"Max Sport Grade: {sport_predicted_grade}")
 
-# Insert another break if needed
+# Insert another break
 st.write('<br>', unsafe_allow_html=True)
 st.markdown('---')
 
