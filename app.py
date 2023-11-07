520def9a03cb18daf6e0f2022e34da6918294f43
Th0m4sG <thomas.georgiou@phiture.com>
2023-11-07 21:37:29 +0200

    reverted form changes

--- app.py
+++ app.py
@@ -37,28 +37,23 @@ sport_scaler_url = "https://github.com/Tetleysteabags/climbing_ml_project/raw/ma
 sport_model = load_pickle_from_url(sport_model_url)
 sport_scaler = load_pickle_from_url(sport_scaler_url)
 
-# define initial values for predicted grades
-bouldering_predicted_grade = "Unknown"
-sport_predicted_grade = "Unknown"
 
 # Create a sidebar for user input
 st.sidebar.header("Enter your climbing stats")
 
-with st.sidebar.form(key='my_form'):
-    height = st.sidebar.number_input("Height (cm)", min_value=0.0, max_value=300.0, value=175.0, step=1.0, key="height")
+def user_input_features():
+    height = st.sidebar.number_input("Height (cm)", min_value=0.0, max_value=300, value=175, step=1, key="height")
     weight = st.sidebar.number_input("Weight (kg)", min_value=0.0, max_value=120.0, value=70.0, step=0.5, key="weight")    
-    max_pullups = st.sidebar.number_input("Max pullups in single go", min_value=0, max_value=50, value=10, step=1, key="max_pullups")
+    max_pullups = st.sidebar.number_input("Max pullups in single go", min_value=0, max_value=100, value=10, step=1, key="max_pullups")
     max_hang_weight = st.sidebar.number_input("Max weight added to hang for 10 seconds from a 20mm edge (kg)", min_value=0.0, max_value=100.0, value=10.0,step=0.1, key="max_hang_weight")
-    max_pullup_weight = st.sidebar.number_input("Max weight added to a single pullup (kg)", min_value=0.0, max_value=100.0, value=10.0, step=0.1, key="max_pullup_weight")
+    max_pullup_weight = st.sidebar.number_input("Max weight added to a single pullup (kg)", min_value=0.0, max_value=100.0, value=10.0, step=0.5, key="max_pullup_weight")
     continuous = st.sidebar.number_input("Continuous hang from 20mm edge (seconds)", min_value=1, max_value=1000, value=30,step=1, key="continuous")
     repeaters = st.sidebar.number_input("7:3 hangs on a 20mm edge (total time in seconds)", min_value=1, max_value=1000, value=120,step=1, key="repeaters")
     trainexp = st.sidebar.number_input("Years of specific training for climbing", min_value=1.0, max_value=50.0, value=3.0,step=0.5, key="trainexp")
     exp = st.sidebar.number_input("Years of climbing experience", min_value=0.0, max_value=50.0, value=5.0,step=0.5, key="exp")
     days = st.sidebar.number_input("Number of days spent climbing each month", min_value=0, max_value=31, value=8,step=1, key="days")
     
-    submit_button = st.form_submit_button(label='Submit metrics')
-        
-if submit_button:
+    
     # Calculate strength-to-weight ratios
     strength_to_weight_pullup =  max_pullups / weight
     strength_to_weight_maxhang = (max_hang_weight + weight) / weight
@@ -78,100 +73,100 @@ if submit_button:
     }
 
     features = pd.DataFrame(data, index=[0])
-    # return strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull, continuous, repeaters, exp, trainexp, days, height, weight
+    return strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull, continuous, repeaters, exp, trainexp, days, height, weight
 
-    # Create separate functions for bouldering and sport features
-    # Collect bouldering-specific features here
-    def bouldering_input_features(strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull,repeaters, exp, days):
-        data_bouldering = {
-            "strength_to_weight_pullup": strength_to_weight_pullup,
-            "strength_to_weight_maxhang": strength_to_weight_maxhang,
-            "strength_to_weight_weightpull": strength_to_weight_weightpull,
-            "continuous": continuous,
-            "exp": trainexp,
-            "days": days
-        }
-        return pd.DataFrame(data_bouldering, index=[0])
-
-    # Collect sport-specific features here                       
-    def sport_input_features(strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull, continuous, repeaters, exp, days):
-        data_sport = {
-            "strength_to_weight_pullup": strength_to_weight_pullup,
-            "strength_to_weight_maxhang": strength_to_weight_maxhang,
-            "strength_to_weight_weightpull": strength_to_weight_weightpull,
-            "continuous": continuous,
-            "repeaters1": repeaters,
-            "exp": exp,
-            "days": days
-        }
-        return pd.DataFrame(data_sport, index=[0])
-
-    # Get the calculated variables from user_input_features
-    # strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull, continuous, repeaters, exp, trainexp, days, height, weight = user_input_features()
-
-    # Pass them to bouldering_input_features and sport_input_features
-    input_df_bouldering = bouldering_input_features(strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull, repeaters, exp, days)
-    input_df_sport = sport_input_features(strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull, continuous, repeaters, exp, days)
-
-    # Apply the same preprocessing used during model training
-    scaled_features_bouldering = bouldering_scaler.transform(input_df_bouldering)
-    scaled_features_sport = sport_scaler.transform(input_df_sport)
-
-    # Mapping functions to produce output in the correct value (e.g. V grade for bouldering and French for sport)
-    # Bouldering
-    conversion_map_boulder = {
-        "<V3": 1,
-        "V3": 2, "V4": 3, "V5": 4,
-        "V6": 5, "V7": 6, "V8": 7,
-        "V9": 8, "V10": 9, "V11": 10,
-        "V12": 11, "V13": 12, "V14": 13,
-        "V15": 14, "V16": 15, "I have not pursued bouldering goals outside in the past year": 0
+# Create separate functions for bouldering and sport features
+# Collect bouldering-specific features here
+def bouldering_input_features(strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull,repeaters, trainexp, days):
+    data_bouldering = {
+        "strength_to_weight_pullup": strength_to_weight_pullup,
+        "strength_to_weight_maxhang": strength_to_weight_maxhang,
+        "strength_to_weight_weightpull": strength_to_weight_weightpull,
+        "continuous": continuous,
+        "trainexp": trainexp,
+        "days": days
     }
+    return pd.DataFrame(data_bouldering, index=[0])
 
-    # Reverse the conversion_map dictionary to map from numerical grade back to V grade
-    inverse_conversion_map_v = {v: k for k, v in conversion_map_boulder.items()}
-
-    # Now define a function to convert numeric grade to V grade
-    def convert_numeric_to_v_grade(numeric_grade):
-        # Round the numeric grade to the nearest integer
-        numeric_grade = round(numeric_grade)
-
-        if numeric_grade in inverse_conversion_map_v:
-            return inverse_conversion_map_v[numeric_grade]
-        else:
-            return "Unknown grade"  # or other string to indicate unknown grade
-
-    # Sport climbing mapping
-    conversion_map_french = {
-        "4c": 1, "5a": 2, "5b": 3, "5c": 4, "6a": 5,
-        "6a+": 6, "6b": 7, "6b+": 8, "6c": 9, "6c+": 10, "7a": 11,
-        "7a+": 12, "7b": 13, "7b+": 14, "7c": 15, "7c+": 16, "8a": 17,
-        "8a+": 18, "8b": 19, "8b+": 20, "8c": 21, "8c+": 22, "9a": 23, "9a+": 24, "0": 0
+# Collect sport-specific features here                       
+def sport_input_features(strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull, continuous, repeaters, exp, trainexp, days):
+    data_sport = {
+        "strength_to_weight_pullup": strength_to_weight_pullup,
+        "strength_to_weight_maxhang": strength_to_weight_maxhang,
+        "strength_to_weight_weightpull": strength_to_weight_weightpull,
+        "continuous": continuous,
+        "repeaters1": repeaters,
+        "exp": exp,
+        "days": days
     }
+    return pd.DataFrame(data_sport, index=[0])
 
-    # Reverse the conversion_map dictionary to map from numerical grade back to French grade sport
-    inverse_conversion_map_f = {v: k for k, v in conversion_map_french.items()}
+# Get the calculated variables from user_input_features
+strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull, continuous, repeaters, exp, trainexp, days, height, weight = user_input_features()
 
-    # Now define a function to convert numeric grade to French grade
-    def convert_numeric_to_f_grade(numeric_grade):
-        # Round the numeric grade to the nearest integer
-        numeric_grade = round(numeric_grade)
+# Pass them to bouldering_input_features and sport_input_features
+input_df_bouldering = bouldering_input_features(strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull, repeaters, trainexp, days)
+input_df_sport = sport_input_features(strength_to_weight_pullup, strength_to_weight_maxhang, strength_to_weight_weightpull, continuous, repeaters, exp, trainexp, days)
 
-        if numeric_grade in inverse_conversion_map_f:
-            return inverse_conversion_map_f[numeric_grade]
-        else:
-            return "Unknown grade"  # or other string to indicate unknown grade
+# Apply the same preprocessing used during model training
+scaled_features_bouldering = bouldering_scaler.transform(input_df_bouldering)
+scaled_features_sport = sport_scaler.transform(input_df_sport)
 
-    # Modeling and predictions to display as output
-    # Make bouldering predictions
-    bouldering_prediction = bouldering_model.predict(scaled_features_bouldering)
-    bouldering_predicted_grade = convert_numeric_to_v_grade(float(bouldering_prediction[0]))
+# Mapping functions to produce output in the correct value (e.g. V grade for bouldering and French for sport)
+# Bouldering
+conversion_map_boulder = {
+    "<V3": 1,
+    "V3": 2, "V4": 3, "V5": 4,
+    "V6": 5, "V7": 6, "V8": 7,
+    "V9": 8, "V10": 9, "V11": 10,
+    "V12": 11, "V13": 12, "V14": 13,
+    "V15": 14, "V16": 15, "I have not pursued bouldering goals outside in the past year": 0
+}
 
-    # Make sport predictions
-    sport_prediction = sport_model.predict(scaled_features_sport)
-    sport_predicted_grade = convert_numeric_to_f_grade(float(sport_prediction[0]))
+# Reverse the conversion_map dictionary to map from numerical grade back to V grade
+inverse_conversion_map_v = {v: k for k, v in conversion_map_boulder.items()}
 
+# Now define a function to convert numeric grade to V grade
+def convert_numeric_to_v_grade(numeric_grade):
+    # Round the numeric grade to the nearest integer
+    numeric_grade = round(numeric_grade)
 
+    if numeric_grade in inverse_conversion_map_v:
+        return inverse_conversion_map_v[numeric_grade]
+    else:
+        return "Unknown grade"  # or other string to indicate unknown grade
+
+# Sport climbing mapping
+conversion_map_french = {
+    "4c": 1, "5a": 2, "5b": 3, "5c": 4, "6a": 5,
+    "6a+": 6, "6b": 7, "6b+": 8, "6c": 9, "6c+": 10, "7a": 11,
+    "7a+": 12, "7b": 13, "7b+": 14, "7c": 15, "7c+": 16, "8a": 17,
+    "8a+": 18, "8b": 19, "8b+": 20, "8c": 21, "8c+": 22, "9a": 23, "9a+": 24, "0": 0
+}
+
+# Reverse the conversion_map dictionary to map from numerical grade back to French grade sport
+inverse_conversion_map_f = {v: k for k, v in conversion_map_french.items()}
+
+# Now define a function to convert numeric grade to French grade
+def convert_numeric_to_f_grade(numeric_grade):
+    # Round the numeric grade to the nearest integer
+    numeric_grade = round(numeric_grade)
+
+    if numeric_grade in inverse_conversion_map_f:
+        return inverse_conversion_map_f[numeric_grade]
+    else:
+        return "Unknown grade"  # or other string to indicate unknown grade
+
+# Modeling and predictions to display as output
+# Make bouldering predictions
+bouldering_prediction = bouldering_model.predict(scaled_features_bouldering)
+bouldering_predicted_grade = convert_numeric_to_v_grade(float(bouldering_prediction[0]))
+
+# Make sport predictions
+sport_prediction = sport_model.predict(scaled_features_sport)
+sport_predicted_grade = convert_numeric_to_f_grade(float(sport_prediction[0]))
+
+# Display the prediction for bouldering
 # Add a message to the main section of the page to inform users where to input their data
 st.header("Please open the sidebar to enter your climbing metrics.")
 # add a break
