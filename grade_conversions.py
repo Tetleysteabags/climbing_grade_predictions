309e7b2997e7c1e2c36532e4c8dcde422f591a9e
Th0m4sG <thomascgeorgiou@gmail.com>
2024-06-26 20:34:33 +0300

    Update utils to use Streamlit secrets for MongoDB connection

--- grade_conversions.py
+++ grade_conversions.py
@@ -15,7 +15,7 @@ conversion_map_french = {
     "4c": 1, "5a": 2, "5b": 3, "5c": 4, "6a": 5, "6a+": 6, "6b": 7, "6b+": 8, 
     "6c": 9, "6c+": 10, "7a": 11, "7a+": 12, "7b": 13, "7b+": 14, "7c": 15, 
     "7c+": 16, "8a": 17, "8a+": 18, "8b": 19, "8b+": 20, "8c": 21, "8c+": 22, 
-    "9a": 23, "9a+": 24, "0": 0
+    "9a": 23, "9a+": 24, "0": 0,  "I have not pursued bouldering goals outside in the past year": 0
 }
 inverse_conversion_map_f = {v: k for k, v in conversion_map_french.items()}
 
--- utils.py
+++ utils.py
@@ -2,17 +2,16 @@ import pickle
 import requests
 import pymongo
 import streamlit as st
+import os
 
 def load_pickle_from_url(url):
     response = requests.get(url)
     return pickle.loads(response.content)
 
 def connect_to_mongodb():
-    secrets = st.secrets["mongo"]
-    conn_str = secrets["conn_str"]
+    conn_str = st.secrets["mongo"]["conn_str"]
     client = pymongo.MongoClient(conn_str)
     db = client.ClimbingGradeFeedback
     collection = db.ClimbingFeedbackStreamlit
     return collection, client
-
 
