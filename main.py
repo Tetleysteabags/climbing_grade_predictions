38c8d105cfe6bee6bf065357287ada9995a6fb73
Th0m4sG <thomascgeorgiou@gmail.com>
2024-07-10 12:51:01 +0300

    add images to streamlit

--- main.py
+++ main.py
@@ -50,9 +50,16 @@ def main():
     sport_predicted_grade = convert_numeric_to_f_grade(sport_prediction)
 
     # Display predictions
+    # Insert an image
+    st.image("https://www.patagonia.com/blog/wp-content/uploads/2020/11/etzel_k_1399_cc_web-1280x720-1.jpg", use_column_width=True)
     st.header("Predicted grades based on your climbing metrics")
+    st.subheader("Open the sidebar on the left to input your strength metrics")
+    st.subheader("Please note that these are just predictions and may not be accurate for everyone.")
     st.subheader(f"Max Boulder Grade: {bouldering_predicted_grade}")
     st.subheader(f"Max Sport Grade: {sport_predicted_grade}")
+    
+    # Insert an image
+    st.image("https://gripped.com/wp-content/uploads/2022/02/LT11_Jess-Talley_adidasTerrex_BrookeRaboutou_3-1.jpg", use_column_width=True)
 
     # Feedback section
     st.header("Feedback to improve future predictions")
