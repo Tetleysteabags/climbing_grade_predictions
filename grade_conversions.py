da34a3de97001db207d9ad588f4d3353c591f1d0
Th0m4sG <103483497+Th0m4sG@users.noreply.github.com>
2025-02-25 19:29:49 +0200

    Added f grade to numeric function

--- grade_conversions.py
+++ grade_conversions.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 import logging
 
-logging.basicConfig(level=logging.INFO)
-
 # Bouldering conversion map
 conversion_map_boulder = {
     "<V3": 1, "V3": 2, "V4": 3, "V5": 4, "V6": 5, "V7": 6, "V8": 7,
@@ -32,6 +30,7 @@ def convert_v_grade_to_numeric(grade):
         return conversion_map_boulder[grade]  # convert the grade to numeric
     else:
         return np.nan  # return NaN for unknown or unclassifiable grades
+
 
 
 def convert_to_french_grade(grade):
@@ -70,6 +69,19 @@ conversion_map_french = {
     '7a+': 12, '7b': 13, '7b+': 14, '7c': 15, '7c+': 16, '8a': 17,
     '8a+': 18, '8b': 19, '8b+': 20, '8c': 21, '8c+': 22, '9a': 23, '9a+': 24, '0': 0
 }
+
+def convert_f_grade_to_numeric(grade):
+    # map of V grades to numerical values
+    conversion_map_french = {
+    '4c': 1, '5a': 2, '5b': 3, '5c': 4, '6a': 5,
+    '6a+': 6, '6b': 7, '6b+': 8, '6c': 9, '6c+': 10, '7a': 11,
+    '7a+': 12, '7b': 13, '7b+': 14, '7c': 15, '7c+': 16, '8a': 17,
+    '8a+': 18, '8b': 19, '8b+': 20, '8c': 21, '8c+': 22, '9a': 23, '9a+': 24, '0': 0
+}
+    if grade in conversion_map_french:  # check if the grade is in the conversion map
+        return conversion_map_french[grade]  # convert the grade to numeric
+    else:
+        return np.nan  # return NaN for unknown or unclassifiable grades
     
 
 # create dictionaries to map experience, season, and days climbing to numeric values so we can use these if needed in our models
